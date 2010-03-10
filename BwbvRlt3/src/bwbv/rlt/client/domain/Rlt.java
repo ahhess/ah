e3ed752f4b5dbdbe59cd4de3dee40da209fd9424
ahhess <andreas.h.hess@googlemail.com>
2010-03-10 16:23:59 +0100

    neu

--- BwbvRlt3/src/bwbv/rlt/client/domain/Rlt.java
+++ BwbvRlt3/src/bwbv/rlt/client/domain/Rlt.java
@@ -2,24 +2,20 @@ package bwbv.rlt.client.domain;
 
 import java.util.Date;
 
-import com.google.gwt.core.client.JavaScriptObject;
+public interface Rlt {
 
-public class Rlt extends JavaScriptObject {
-
-	protected Rlt() {}
-
-	public final native String getId() 			/*-{ return this.id; }-*/;
-	public final native String getRltKat() 		/*-{ return this.rltkat; }-*/;
-	public final native String getRltStatus() 	/*-{ return this.rltstatus; }-*/;
-	public final native String getKurzbez() 	/*-{ return this.kurzbez; }-*/;
-	public final native String getTurnierbez() 	/*-{ return this.turnierbez; }-*/;
-	public final native String getOrt() 		/*-{ return this.ort; }-*/;
-	public final native String getHalle() 		/*-{ return this.halle; }-*/;
-	public final native String getAdresse() 	/*-{ return this.adresse; }-*/;
-	public final native String getDatumtext() 	/*-{ return this.datumtext; }-*/;
-	public final native Date getDatum() 		/*-{ return this.datum; }-*/;
-	public final native Date getCreated() 		/*-{ return this.created; }-*/;
-	public final native Date getMeldeschluss() 	/*-{ return this.meldeschluss; }-*/;
-	public final native String[] getDiszs() 	/*-{ return this.diszs; }-*/;
-
+	String getId() 			;
+	String getRltKat() 		;
+	String getRltStatus() 	;
+	String getKurzbez() 	;
+	String[] getDiszs() 	;
+	
+	String getTurnierbez() 	;
+	String getOrt() 		;
+	String getHalle() 		;
+	String getAdresse() 	;
+	String getDatumtext() 	;
+	Date getDatum() 		;
+	Date getCreated() 		;
+	Date getMeldeschluss() 	;
 }
