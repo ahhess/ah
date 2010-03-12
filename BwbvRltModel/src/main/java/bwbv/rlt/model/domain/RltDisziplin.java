4b06751bd97a93c8d3336f6c2848c9556e685a8e
ahhess <andreas.h.hess@googlemail.com>
2010-03-12 17:20:46 +0100

    neu

---
+++ BwbvRltModel/src/main/java/bwbv/rlt/model/domain/RltDisziplin.java
@@ -0,0 +1,6 @@
+package bwbv.rlt.model.domain;
+
+@SuppressWarnings("serial")
+public class RltDisziplin extends Detail {
+
+}
---
+++ BwbvRltModel/src/main/java/bwbv/rlt/model/domain/RltKat.java
@@ -0,0 +1,6 @@
+package bwbv.rlt.model.domain;
+
+@SuppressWarnings("serial")
+public class RltKat extends Detail {
+
+}
