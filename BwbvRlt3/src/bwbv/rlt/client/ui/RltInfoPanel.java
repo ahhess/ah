30008d2346f29f2a36601c80fbd0992bc56baaea
andy@home <andreas.h.hess@googlemail.com>
2010-05-04 01:13:13 +0200

    mit grid

--- BwbvRlt3/src/bwbv/rlt/client/ui/RltInfoPanel.java
+++ BwbvRlt3/src/bwbv/rlt/client/ui/RltInfoPanel.java
@@ -3,8 +3,7 @@ package bwbv.rlt.client.ui;
 import bwbv.rlt.model.domain.Rlt;
 
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.FlexTable;
-import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 
 public class RltInfoPanel extends Composite {
@@ -15,10 +14,24 @@ public class RltInfoPanel extends Composite {
 		horizontalPanel.setHorizontalAlignment(HorizontalPanel.ALIGN_CENTER);
 		horizontalPanel.setWidth("25%");
 
-		//TODO
-		horizontalPanel.add(new HTML(rlt.getKurzBez()));
+		Grid grid = new Grid(7,2);
+		horizontalPanel.add(grid);
 		
-//		FlexTable table = new FlexTable();
+		int i=0;
+		grid.setText(i, 0, "RLT:");
+		grid.setText(i++, 1, rlt.getLangBez());
+		grid.setText(i, 0, "Kategorie:");
+		grid.setText(i++, 1, rlt.getKat()==null?"":rlt.getKat().getLangBez());
+		grid.setText(i, 0, "Ort:");
+		grid.setText(i++, 1, rlt.getOrt());
+		grid.setText(i, 0, "Halle:");
+		grid.setText(i++, 1, rlt.getHalle());
+		grid.setText(i, 0, "Adresse:");
+		grid.setText(i++, 1, rlt.getAdresse());
+		grid.setText(i, 0, "Datum:");
+		grid.setText(i++, 1, rlt.getDatumtext());
+		grid.setText(i, 0, "Meldeschluss:");
+		grid.setText(i++, 1, rlt.getMeldeschluss()==null?"":rlt.getMeldeschluss().toString());
 		
 		initWidget(horizontalPanel);
 	}
